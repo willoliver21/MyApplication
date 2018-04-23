166ce4b029ad72f50afd65049538ab1a4a1d8f9f
willoliver21 <33660533+willoliver21@users.noreply.github.com>
2018-04-23 20:40:39 -0300

    Aula(Fragment)
    Implementado uma nova activity com fragment na aula 23/04 com Rogerio.

---
+++ app/src/main/java/com/example/willo/myapplication/ExemploFragmentPrincipalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.willo.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExemploFragmentPrincipalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exemplo_fragment_principal)
+    }
+}
